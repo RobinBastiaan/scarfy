0effa0e598c46108267b9dba479843a2498cc1a7
RobinBastiaan <robinbastiaan@gmail.com>
2021-10-22 00:42:03 +0200

    :lipstick: Add base template setup

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            @yield('main')
+        </div>
+    </main>
+@endsection
