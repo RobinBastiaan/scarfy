43f8ce9826965d64b67793d4cbfc122ff8d4d307
RobinBastiaan <robinbastiaan@gmail.com>
2021-11-12 13:47:18 +0100

    :globe_with_meridians: Improve translations with Laravel-Lang/lang

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -11,7 +11,9 @@
                 <div class="md:w-2/3 w-full px-3 flex flex-row flex-wrap">
                     <div class="w-full text-right text-gray-700 font-semibold relative pt-3 md:pt-0">
                         <div class="text-2xl text-gray-700 leading-tight">{{ $group->name }}</div>
-                        <div class="text-normal text-gray-300 hover:text-gray-400 cursor-pointer"><span class="border-b border-dashed border-gray-500 pb-1">{{ $group->city }}, {{ $group->country }}</span></div>
+                        <div class="text-normal text-gray-300 hover:text-gray-400 cursor-pointer">
+                            <span class="border-b border-dashed border-gray-500 pb-1">{{ $group->city }}, {{ __($group->country) }}</span>
+                        </div>
                         <div class="text-sm text-gray-300 hover:text-gray-400 cursor-pointer md:absolute pt-3 md:pt-0 bottom-0 right-0">{{ $group->website }}</div>
                     </div>
                 </div>
