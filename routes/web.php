fd9803218371d62a145a8062d9d65263329c1ace
RobinBastiaan <robinbastiaan@gmail.com>
2021-11-17 21:43:02 +0100

    :sparkles: Add about page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\ScarfController;
 use App\Http\Controllers\ScoutGroupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', LandingPageController::class)->name('landing_page');
+Route::get('/about', AboutController::class)->name('about');
 
 Route::resources([
     'scarves' => ScarfController::class,
