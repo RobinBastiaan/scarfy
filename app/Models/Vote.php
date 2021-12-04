6aa19203747a482f904f24984f45fac51304b61e
RobinBastiaan <robinbastiaan@gmail.com>
2021-12-04 22:40:18 +0100

    :card_file_box: Create Vote model

---
+++ app/Models/Vote.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Vote extends Model
+{
+    use HasFactory;
+
+    public function voteable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
---
+++ database/migrations/2021_12_04_213355_create_votes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->morphs('voteable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes');
+    }
+}
