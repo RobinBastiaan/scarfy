5120f64373b8096aa4a79fa191ecbd43fb6bb4e6
RobinBastiaan <robinbastiaan@gmail.com>
2022-01-01 18:27:03 +0100

    :lipstick: Add Bootstrap 5

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .css('resources/css/app.css', 'public/css')
+    .sass('resources/scss/app.scss', 'public/css')
     .version();
