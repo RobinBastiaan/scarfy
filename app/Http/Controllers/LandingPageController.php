068d1567893b07f8621cb1d51dfb02b3a622dd6e
RobinBastiaan <robinbastiaan@gmail.com>
2022-12-18 00:48:20 +0100

    :zap: Add caching to landing page queries

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Scarf;
 use App\Models\ScoutGroup;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class LandingPageController extends Controller
 {
     public function __invoke()
     {
+        $ttl = 3600;
+
+        $totalScarves = Cache::remember('totalScarves', $ttl, fn() => Scarf::query()->count());
+        $totalScoutGroups = Cache::remember('totalScoutGroups', $ttl, fn() => ScoutGroup::query()->count());
+        $recentAdditions = Cache::remember('recentAdditions', $ttl, fn() => ScoutGroup::recentAdditions()->get());
+
         return view('landing-page', [
-            'totalScarves'      => Scarf::query()->count(),
-            'totalScoutGroups'  => ScoutGroup::query()->count(),
-            'recentAdditions'   => ScoutGroup::recentAdditions()->get(),
+            'totalScarves'      => $totalScarves,
+            'totalScoutGroups'  => $totalScoutGroups,
+            'recentAdditions'   => $recentAdditions,
             'showScoutScarfDay' => $this->shouldShowScoutScarfDay(),
         ]);
     }
